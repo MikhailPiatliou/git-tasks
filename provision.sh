5c432f5db2fb0faf42116b5a26ce9412a1871c00
Darya Zhukova <darya_zhukova@epam.com>
2018-07-19 16:52:34 +0300

    nginx config added

--- provision.sh
+++ provision.sh
@@ -1,5 +1,40 @@
 #! /usr/bin/bash
 # beta 0.1
+###########Nginx setup############
+yum -y install nginx
+cat <<EOF > /etc/nginx/conf.d/server.conf
+server {
+        listen       80 default_server;
+        listen       [::]:80 default_server;
+
+        server_name  jenkins;
+        add_header backend_srv jenkins;
+        #charset koi8-r;
+location / {
+           proxy_pass http://192.168.15.2:8080;
+        }
+}
+
+EOF
+
+cat <<EOF > /etc/nginx/nginx.conf
+worker_processes  1;
+events {
+    worker_connections  1024;
+}
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+    sendfile        on;
+    keepalive_timeout 65;
+include /etc/nginx/conf.d/*.conf;
+
+}
+
+EOF
+
+
+
 
 cp /vagrant/jenkins.service /etc/systemd/system/
 
