2f7096cb4770acf90b8aae51f18970bacfffe9ee
Aliaksandr <aranich@ya.ru>
2018-07-19 15:20:06 +0300

    Added creation of an init.d file in the provision script

--- jenkins.sh
+++ jenkins.sh
@@ -8,8 +8,19 @@ systemctl enable nginx
 sed -i -e 's/\(^\s*location \/ {\)/\1\n\t    proxy_pass http:\/\/127.0.0.1:8080;/' /etc/nginx/nginx.conf
 systemctl start nginx
 useradd jenkins
+cat << EOF > /etc/system.d/system/jenkins.service
+[Unit]
+Description=Jenkins Daemon
 
+[Service]
+Environment=JENKINS_DIR=/opt/jenkins/bin
+Environment=JENKINS_HOME=/opt/jenkins/master
+ExecStart=/usr/bin/java -D${JENKINS_HOME} -jar ${JENKINS_DIR}/jenkins.war
+User=jenkins
+
+[Install]
+WantedBy=multi-user.target
+EOF
 systemctl daemon-reload
 systemctl enable jenkins
-systemctl start jenkins 
-
+systemctl start jenkins
