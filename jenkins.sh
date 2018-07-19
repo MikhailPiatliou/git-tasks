5f69430307d73d5769cb65b259885d4fc7c04307
Aliaksandr <aranich@ya.ru>
2018-07-19 13:58:36 +0300

    Added jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+yum install -y java-1.8.0-openjdk nginx
+mkdir -p /opt/jenkins/bin
+cd /opt/jenkins/bin
+wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+systemctl enable nginx
+sed -i -e 's/\(^\s*location \/ {\)/\1\n\t    proxy_pass http:\/\/127.0.0.1:8080;/' /etc/nginx/nginx.conf
+systemctl start nginx
