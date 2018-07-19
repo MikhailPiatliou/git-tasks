a26de87c6d647d9455ae5315abcf0952c774052b
Aliaksandr <aranich@ya.ru>
2018-07-19 16:23:15 +0300

    Fixed an issue with variables in systemd script

--- jenkins.sh
+++ jenkins.sh
@@ -8,14 +8,14 @@ systemctl enable nginx
 sed -i -e 's/\(^\s*location \/ {\)/\1\n\t    proxy_pass http:\/\/127.0.0.1:8080;/' /etc/nginx/nginx.conf
 systemctl start nginx
 useradd jenkins
-cat << EOF > /etc/system.d/system/jenkins.service
+cat << EOF > /etc/systemd/system/jenkins.service
 [Unit]
 Description=Jenkins Daemon
 
 [Service]
 Environment=JENKINS_DIR=/opt/jenkins/bin
 Environment=JENKINS_HOME=/opt/jenkins/master
-ExecStart=/usr/bin/java -D${JENKINS_HOME} -jar ${JENKINS_DIR}/jenkins.war
+ExecStart=/usr/bin/java -D\${JENKINS_HOME} -jar \${JENKINS_DIR}/jenkins.war
 User=jenkins
 
 [Install]
