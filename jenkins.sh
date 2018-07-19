687be2f19bc8c2872f22b1a7be1c200f8d4ecdee
EugenePapkou <eugene.papkou@gmail.com>
2018-07-19 15:11:46 +0300

    provisioning script

--- jenkins.sh
+++ jenkins.sh
@@ -7,3 +7,9 @@ wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
 systemctl enable nginx
 sed -i -e 's/\(^\s*location \/ {\)/\1\n\t    proxy_pass http:\/\/127.0.0.1:8080;/' /etc/nginx/nginx.conf
 systemctl start nginx
+useradd jenkins
+
+systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins 
+
