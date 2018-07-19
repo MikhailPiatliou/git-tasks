eeeb23ea5aa0e177cdbf53319f1af897b426289b
MikhailPiatliou <mishok26@gmail.com>
2018-07-19 16:22:48 +0300

    Full changes

--- configureJenkins.sh
+++ configureJenkins.sh
@@ -5,11 +5,34 @@ yum -y --nogpgcheck install java mc net-tools nano screen pcre-devel zlib-devel;
 
 #installing jenkins
 screen -t jenkins -dm bash -c '
-mkdir /home/vagrant/jenk;
-cd $_;
+mkdir /opt/jenkins/ && cd $_;
+mkdir master
+mkdir bin
 wget https://mirrors.tuna.tsinghua.edu.cn/jenkins/war-stable/latest/jenkins.war;
-wall "Jenkins is installed.";
-java -jar ./jenkins.war;
+useradd -d /opt/jenkins/master jenkins;
+mv jenkins.war bin/;
+cat << EOF > /etc/systemd/system/jenkins.service
+[Unit]
+Description=Jenkins Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/java -jar /opt/jenkins/bin/jenkins.war
+Restart=always
+User=jenkins
+
+[Install]
+WantedBy=multi-user.target
+EOF
+export JENKINS_HOME=/opt/jenkins/master
+export JENKINS_DIR=/opt/jenkins/bin
+PATH=$PATH:$JENKINS_HOME:$JENKINS_DIR
+export PATH
+# cp /vagrant/jenkins.service /etc/systemd/system/;
+systemctl daemon-reload;
+systemctl start jenkins;
+wall "Jenkins was installed and started.";
 '
 
 #installing nginx
@@ -17,7 +40,7 @@ screen -t "nginx" -dm bash -c 'mkdir /home/vagrant/nginx;
 cd $_;
 wget http://nginx.org/download/nginx-1.14.0.tar.gz;
 tar -xf ./nginx-1.14.0.tar.gz; cd nginx-1.14.0;
-wall "nginx is installed";
+wall "nginx was installed";
 ./configure; make; make install;
 touch /var/log/nginx/jenkins.access.log;
 rm /usr/local/nginx/conf/nginx.conf;
