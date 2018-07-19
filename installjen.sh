a9a9f0af1ed616293df9cc2adc70e19397b66e66
Eletriclimbo <32715736+Eletriclimbo@users.noreply.github.com>
2018-07-19 16:52:04 +0300

    Delete installjen.sh

--- installjen.sh
+++
@@ -1,6 +0,0 @@
-yum install -y java
-sudo yum install -y nginx
-rm /home/vagrant/nginx/conf/nginx.conf
-cp /vagrant/nginx.conf /home/vagrant/nginx/conf/nginx.conf
-wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war
-java -jar jenkins.war &
